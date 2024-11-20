b87bc1989bbef1b78ced77b2844e03870113bb03
Radoslav Kalushkov <radi_kalu6kov@abv.bg>
2024-11-20 17:23:56 +0200

    Updating the book module passing all unit tests

--- Template/src/models/book.js
+++ Template/src/models/book.js
@@ -2,4 +2,76 @@ import { bookGenre } from '../common/book-genre.js';
 
 export class Book {
 
+    #title;
+    #genre;
+    #pages;
+    #authors;
+
+    constructor(initialTitle, initialGenre, initialPages, initialAuthors) {
+
+      this.title = initialTitle;
+      this.genre = initialGenre;
+      this.pages = initialPages;
+      this.#authors = [];
+      this.authors = initialAuthors;
+
+    }
+
+    get title() {
+      return this.#title;
+    }
+    set title(value) {
+      if (typeof value === 'string' && value !== '' && value.length >= 1 && value.length <= 255) {
+        this.#title = value;
+      } else {
+        throw new Error('This title is not a valid title!');
+      }
+
+    }
+
+    get genre() {
+      return this.#genre;
+    }
+    set genre(value) {
+      if (!Object.values(bookGenre).includes(value)) {
+        throw new Error('This genre is not valid!');
+      }
+      this.#genre = value;
+    }
+
+    get pages() {
+      return this.#pages;
+    }
+    set pages(value) {
+      if (value > 0 && Number.isInteger(value)) {
+        this.#pages = value;
+      } else {
+        throw new Error('The number of pages is not a valid number!');
+      }
+    }
+
+    get authors() {
+      return this.#authors.toString();
+    }
+    set authors(value) {
+      if (value === '') {
+        throw new Error('This author name is not valid!');
+      } if (!value.includes(',')) {
+        if (value.split(' ').join('').length >= 1 && value.split(' ').join('').length <= 40) {
+          this.#authors.push((Array.from(value).map(e => e.trim())).join(' '));
+        } else {
+          throw new Error('This author name is not valid!');
+        }
+      } if (value.includes(',')) {
+
+        if (value.split(',').every(e => e.length >= 0 && e.length <= 40)) {
+          this.#authors.push((value.split(',').filter(e => e !== ',' && e !== '')).map(el => el.trim()).join(','));
+        } else {
+          throw new Error('This author name is not valid!');
+        }
+      }
+
+
+    }
+
 }

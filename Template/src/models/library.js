5c4c8967cd81441a14914c9bec55e00f44626ce7
Radoslav Kalushkov <radi_kalu6kov@abv.bg>
2024-11-20 19:58:19 +0200

    Implementing the Library module

--- Template/src/models/library.js
+++ Template/src/models/library.js
@@ -1,5 +1,29 @@
 import { libraryShelf } from '../common/library-shelf.js';
 
 export class Library {
+    _shelves;
 
+    constructor() {
+      this._shelves = {};
+    }
+
+    addBook(book, shelf) {
+      if (Object.values(libraryShelf).includes(shelf)) {
+        this._shelves[shelf] = [];
+        this._shelves[shelf].push(book);
+
+      } else {
+        throw new Error('There is no such shelf in the library!');
+      }
+
+    };
+    printBooks() {
+      if (Object.keys(this._shelves).length === 0) {
+        return `No books added`;
+      }
+      return Object.keys(this._shelves)
+        .map(key => `== ${libraryShelf[key]} ==` + `\n` + `${this._shelves[key].map(v => `${v.title}, by ${v.authors}, ${v.pages} pages`)}` + `\n`).join(`\n`).trim();
+    };
 }
+
+

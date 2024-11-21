c806b2f864437bcd0a34da2b01aa74629ef29390
Radoslav Kalushkov <radi_kalu6kov@abv.bg>
2024-11-21 16:01:18 +0200

    Added some more tests in order to fix a mistake with regards to the Library model

--- Template/src/main.js
+++ Template/src/main.js
@@ -7,10 +7,14 @@ import { Library } from './models/library.js';
 
 const book1 = new Book('IT', bookGenre.Horror, 600, 'Steven King');
 const book2 = new Book('Startide Rising', bookGenre.SciFi, 345, 'David Brin');
+const book3 = new Book('Harry Potter', bookGenre.Fantasy, 700, 'j.k Rowling');
+const book4 = new Book('Game of Thrones', bookGenre.Fantasy, 100000, 'J.R.R. Martin');
 
 const library = new Library();
 
 library.addBook(book1, libraryShelf.Clowns);
 library.addBook(book2, libraryShelf.Ocean);
+library.addBook(book3, libraryShelf.Ocean);
+library.addBook(book4, libraryShelf.Elemental);
 
 console.log(library.printBooks());
